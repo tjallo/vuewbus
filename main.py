b4308c83669b33222d9861b27c50679eb891924b
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-06-24 18:36:03 +0200

    added start scripts, started working on api

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # Import Dependencies 
-import fastapi
+from fastapi import FastAPI
 
 # Import Local Dependencies
+from api.game import cards as cards
+
+#Setup FastAPI
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
