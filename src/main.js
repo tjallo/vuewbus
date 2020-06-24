846f2bdd6d723d2620a9c549258a09d98742b807
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-06-24 19:24:09 +0200

    geen zin meer voor vandaag

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
+  axios,
+  VueAxios,
   router,
   vuetify,
   render: h => h(App)
