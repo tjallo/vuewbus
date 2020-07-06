319d6d3221cc3885605f457c423cfa0a51d9a9ce
Tjalle <31376864+tjallo@users.noreply.github.com>
2020-07-06 20:45:48 +0200

    schijt aan async, dag

--- src/views/Round1.vue
+++ src/views/Round1.vue
@@ -1,23 +1,48 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <v-btn @click="getInfo">getInfo</v-btn>
+    <h1>Player: {{player}}</h1>
+    <h1>Round: {{round}}</h1>
+    <h1>Cardname: {{cardname}}</h1>
+  </v-app>
 </template>
 
 <script>
-import func from '../../vue-temp/vue-editor-bridge';
-const axios = require('axios')
-
-function increment() {
-    return "Test"
-}
-
+const axios = require("axios");
 
 export default {
   data() {
     return {
-      players: ['Henk']
+      player: "",
+      round: 0,
+      cardname: ""
     };
   },
+  methods: {
+    getInfo: function() {
+      var player, round, cardname;
 
+      var config = {
+        method: "get",
+        url: "http://localhost:8000/round1/1/",
+        headers: {}
+      };
+
+      axios(config)
+        .then(function(response) {
+          const data = response.data;
+          console.log(data);
+          this.player = data[0];
+          this.round = data[1];
+          this.cardname = data[2];
+          console.log(player, round, cardname);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+
+      console.log(this.player);
+    }
   }
 };
 </script>
